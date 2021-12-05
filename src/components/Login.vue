09a6194739bc0ca84a5c67afc1195d3bbfd355cd
Phong phò <43817495+binh1672000@users.noreply.github.com>
2021-12-05 22:24:33 +0700

    ada

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+     <form @submit.prevent="handleSubmit">
+        <h3>Login</h3>
+
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" class="form-contrl" v-model="email" placeholder="Email"/>
+        </div>
+
+         <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-contrl" v-model="password" placeholder="Password"/>
+        </div>
+
+        <button class="btn btn-primary btn-block">Login</button>
+     </form> 
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default{
+    name: 'Login',
+
+    data(){
+        return {
+            email:'',
+            password : '',
+        }
+    },
+    methods: {
+     async  handleSubmit(){
+         const response = await axios.post('login',{
+             email : this.email ,
+             password : this.password,
+         });
+     localStorage.setItem('token', response.data.token);
+     this.$router.push('/');
+        }
+    }
+}
+
+</script>
+
