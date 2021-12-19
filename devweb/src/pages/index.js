11028c6ceb9808035061c889852e9395a9a2ef72
l1ghtg3m <huy.189.189@gmail.com>
2021-12-19 21:08:27 +0700

    Searcher

--- devweb/src/pages/index.js
+++ devweb/src/pages/index.js
@@ -4,4 +4,5 @@ import UserCard from "../pages/UserProfile/UserCard.vue";
 // Forms
 import EditProfileForm from "../pages/UserProfile/EditProfileForm.vue";
 // icon link https://mui.com/components/material-icons/
+// vue doc link https://www.creative-tim.com/vuematerial/getting-started/
 export { UserCard, EditProfileForm};
