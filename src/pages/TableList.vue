4645ded30543721ad116da3e16fee08582b99142
Phong phò <43817495+binh1672000@users.noreply.github.com>
2021-12-26 20:25:26 +0700

    1

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -20,14 +20,18 @@
       <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
       >
-        <md-card-content>
-          <Replaceaccount></Replaceaccount>
-        </md-card-content>
-       
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Thay đổi tài khoản</h4>
+            <p class="category">Thay đổi tên hoặc quyền</p>
+          </md-card-header>
+          <md-card-content>
+            <Replaceaccount table-header-color="green"></Replaceaccount>
+          </md-card-content>
+          >
+        </md-card>
       </div>
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
-      ></div>
+      
     </div>
   </div>
 </template>
--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -3,12 +3,24 @@
     <div class="md-layout">
       <div class="md-layout-item md-medium-size-100 md-size-100">
         <edit-profile-form data-background-color="green"> </edit-profile-form>
-         <md-card-content>
+       
+      </div>
+       <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
+      >
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Khai báo</h4>
+            <p class="category">Khai báo theo hộ và các nhân</p>
+          </md-card-header>
+          <md-card-content>
           <Khaibao></Khaibao>
         </md-card-content>
         <md-card-content>
           <Khaibaocanhan></Khaibaocanhan>
         </md-card-content>
+          >
+        </md-card>
       </div>
     </div>
   </div>
