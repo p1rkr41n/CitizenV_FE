8b4a73061e08b24fb24ff6e6a3a4c0a01340b53b
Phong phò <43817495+binh1672000@users.noreply.github.com>
2021-12-26 20:32:29 +0700

    Update UserProfile.vue

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -3,23 +3,32 @@
     <div class="md-layout">
       <div class="md-layout-item md-medium-size-100 md-size-100">
         <edit-profile-form data-background-color="green"> </edit-profile-form>
-       
       </div>
-       <div
+      <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
       >
         <md-card>
           <md-card-header data-background-color="green">
-            <h4 class="title">Khai báo</h4>
-            <p class="category">Khai báo theo hộ và các nhân</p>
+            <h4 class="title">Khai báo theo hộ gia đình</h4>
+            <p class="category">Khai báo theo hộ gia đình</p>
           </md-card-header>
           <md-card-content>
-          <Khaibao></Khaibao>
-        </md-card-content>
-        <md-card-content>
-          <Khaibaocanhan></Khaibaocanhan>
-        </md-card-content>
-          >
+            <Khaibao></Khaibao>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
+      >
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Khai báo cá nhân</h4>
+            <p class="category">Khai báo theo các nhân</p>
+          </md-card-header>
+
+          <md-card-content>
+            <Khaibaocanhan></Khaibaocanhan>
+          </md-card-content>
         </md-card>
       </div>
     </div>
@@ -27,14 +36,14 @@
 </template>
 
 <script>
-import { EditProfileForm} from "@/pages";
+import { EditProfileForm } from "@/pages";
 import Khaibao from "../components/Tables/Khaibao.vue";
 import Khaibaocanhan from "../components/Tables/Khaibaocanhan.vue";
 
 export default {
   components: {
     EditProfileForm,
-      Khaibaocanhan,
+    Khaibaocanhan,
     Khaibao,
   },
   data() {
@@ -55,7 +64,6 @@ export default {
       this.$router.push("/");
     },
     //get data from api
-
   },
 };
 </script>
